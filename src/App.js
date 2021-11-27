704cabb95c25b7726567fa67a019e479a1942dd9
Camila Rondinini <crondinini@gmail.com>
2021-11-27 22:45:08 +0100

    update contract and add small functionalities

--- src/App.js
+++ src/App.js
@@ -8,6 +8,37 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 // const OPENSEA_LINK = '';
 // const TOTAL_MINT_COUNT = 50;
+const CONTRACT_ADDRESS = "0x5e9a5EFc4249153d8061F258b34C6FCaF121b0A3";
+
+    // Setup our listener.
+const setupEventListener = async () => {
+  // Most of this looks the same as our function askContractToMintNft
+  try {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      // Same stuff again
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, zenFT.abi, signer);
+
+      // THIS IS THE MAGIC SAUCE.
+      // This will essentially "capture" our event when our contract throws it.
+      // If you're familiar with webhooks, it's very similar to that!
+      connectedContract.on("NewZenFTMinted", (from, tokenId) => {
+        console.log(from, tokenId.toNumber())
+        alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+      });
+
+      console.log("Setup event listener!")
+
+    } else {
+      console.log("Ethereum object doesn't exist!");
+    }
+  } catch (error) {
+    console.log(error)
+  }
+};
 
 const checkIfWalletIsConnected = async (setAccount) => {
   /*
@@ -22,6 +53,16 @@ const checkIfWalletIsConnected = async (setAccount) => {
     console.log("We have the ethereum object", ethereum);
   }
 
+  let chainId = await ethereum.request({ method: 'eth_chainId' });
+  console.log("Connected to chain " + chainId);
+
+  // String, hex code of the chainId of the Rinkebey test network
+  const rinkebyChainId = "0x4";
+  if (chainId !== rinkebyChainId) {
+    alert("You are not connected to the Rinkeby Test Network!");
+    return;
+  }
+
   const accounts = await ethereum.request({ method: 'eth_accounts' });
 
     /*
@@ -30,7 +71,8 @@ const checkIfWalletIsConnected = async (setAccount) => {
       if (accounts.length !== 0) {
         const account = accounts[0];
         console.log("Found an authorized account:", account);
-        setAccount(account)
+        setAccount(account);
+        setupEventListener();
       } else {
           console.log("No authorized account found")
       }
@@ -38,10 +80,13 @@ const checkIfWalletIsConnected = async (setAccount) => {
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [openSeaLink, setOpenSeaLink] = useState("");
+  const [loading, setLoading] = useState(null);
 
   useEffect(() => {
     checkIfWalletIsConnected(setCurrentAccount);
   }, [])
+
 
   const connectWallet = async () => {
     try {
@@ -56,7 +101,6 @@ const App = () => {
       * Fancy method to request access to account.
       */
       const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-
       /*
       * Boom! This should print out public address once we authorize Metamask.
       */
@@ -69,8 +113,8 @@ const App = () => {
   };
 
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "0x4c3d29df80ed54d065e428b2d5fedecede61c511";
       try {
+        setLoading(true);
         const { ethereum } = window;
 
         if (ethereum) {
@@ -84,12 +128,20 @@ const App = () => {
           console.log("Mining...please wait.")
           await nftTxn.wait();
 
+          // connectedContract.on("NewZenFTMinted", (from, tokenId) => {
+          //   console.log(from, tokenId.toNumber())
+          //   alert(`Hey there! We've minted your NFT. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: <https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}>`)
+          //   setOpenSeaLink(`https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`);
+          //   setLoading(false);
+          // });
+
           console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
 
         } else {
           console.log("Ethereum object doesn't exist!");
         }
       } catch (error) {
+        setLoading(false);
         console.log(error)
       }
   };
@@ -100,6 +152,7 @@ const App = () => {
       Connect to Wallet
     </button>
   );
+
 
   return (
     <div className="App">
@@ -116,6 +169,10 @@ const App = () => {
               Mint NFT
             </button>
           )}
+          {typeof loading === 'boolean' ?
+          (<p className="sub-text">{loading ? "Loading..." : `Here's your link: ${openSeaLink}`}</p>)
+          : null
+          }
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
